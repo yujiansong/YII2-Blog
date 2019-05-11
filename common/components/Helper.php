d86728a247ef4ad0746a3234d2bd63b07862ab83
yujiansong <yujiansong6@163.com>
2019-05-11 17:32:32 +0800

    * 修改配置

---
+++ common/components/Helper.php
@@ -0,0 +1,10 @@
+<?php
+namespace common\components;
+
+class Helper
+{
+    public static function checkedMobile($mobile)
+    {
+        return $mobile;
+    }
+}
--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,24 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'helper' => [
+            'class' => 'common\components\Helper',
+            'property' => '123',
+        ],
+        // 配置数据库
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=192.168.15.130;dbname=advanced',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 24*3600,
+            'schemaCache' => 'cache',
+        ],
     ],
+    // 配置语言
+    'language'=>'zh-CN',
+    // 配置时区
+    'timeZone'=>'Asia/Chongqing',
 ];
